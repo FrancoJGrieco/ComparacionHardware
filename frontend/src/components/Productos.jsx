2a18d90ece6c5f9b67d663063e94f18c8223d5f5
FrancoJGrieco <francojesusgrieco@gmail.com>
2025-03-10 23:50:52 -0300

    Se agregaron fuentes en index

--- frontend/src/components/Productos.jsx
+++ frontend/src/components/Productos.jsx
@@ -1,13 +1,39 @@
+import { Typography, CardActionArea, CardMedia, Card, Box } from '@mui/material'
+
 /* eslint-disable react/prop-types */
 export default function Productos({ producto }) {
     return (
-        <section className='pr-section' key={producto._id}>
-            <img src={producto.imagen} style={{ width: '128px', height: '128px' }}></img>
-            <div>
-                <p>Local: {producto.local}</p>
-                <p>Producto: {producto.producto}</p>
-                <p>Precio: {producto.precio}</p>
-            </div>
-        </section>
+        <Card className='pr-section' key={producto._id}>
+            <CardActionArea>
+                <CardMedia
+                    component='img'
+                    height='200'
+                    width='128'
+                    image={producto.imagen}
+                    alt='producto_imagen'
+                ></CardMedia>
+                <Box>
+                    <Typography variant="subtitle1">
+                    Local: {producto.local}
+                    </Typography>
+
+                    <Typography variant="subtitle2">
+                    Producto: {producto.producto}
+                    </Typography>
+
+                    <Typography variant="subtitle2">
+                    Precio: {producto.precio}
+                    </Typography>
+                </Box>
+            </CardActionArea>
+        </Card>
+        // <section className='pr-section' key={producto._id}>
+        //     <img src={producto.imagen} style={{ width: '128px', height: '128px' }}></img>
+        //     <div>
+        //         <p>Local: {producto.local}</p>
+        //         <p>Producto: {producto.producto}</p>
+        //         <p>Precio: {producto.precio}</p>
+        //     </div>
+        // </section>
     )
 }
